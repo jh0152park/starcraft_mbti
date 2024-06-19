047e189de4ad7f3b34e95d785b3637f83dc7d069
jh0152park <jh0152park@naver.com>
2024-06-19 18:30:05 +0900

    replace favicon

--- starcraft-mbti/app/layout.tsx
+++ starcraft-mbti/app/layout.tsx
@@ -8,6 +8,9 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
     title: "Starcraft Unit MBTI",
     description: "Guess who has same MBTI as me!!?",
+    icons: {
+        icon: "/resource/images/logo/favicon.ico",
+    },
 };
 
 export default function RootLayout({
