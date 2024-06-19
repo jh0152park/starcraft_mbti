153571963507755adf7dd3707e9d6528a4ea3c83
jh0152park <jh0152park@naver.com>
2024-06-19 23:08:57 +0900

    added not-found page

---
+++ starcraft-mbti/app/not-found.tsx
@@ -0,0 +1,26 @@
+import LinkButton from "@/components/button/link-button";
+import Image from "next/image";
+
+export default function NotFound() {
+    return (
+        <div className="flex items-center justify-center w-full h-screen ">
+            <div className=" max-w-[2000px] w-full h-screen relative flex flex-col items-center justify-center ">
+                <Image
+                    fill
+                    src="/resource/images/background/not_found_background.png"
+                    alt="Notfound-Background-Image"
+                    className="-z-10"
+                />
+
+                <div className="flex flex-col items-center justify-center *:font-bold *:sm:text-4xl *:drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)] text-xl text-center gap-10">
+                    <span>Commander, something seems to be broken.</span>
+                    <span>We must pull back!</span>
+                </div>
+
+                <div className="absolute top-3/4">
+                    <LinkButton title="Pull Back" path="/" />
+                </div>
+            </div>
+        </div>
+    );
+}
